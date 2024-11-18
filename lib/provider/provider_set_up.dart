3d41e3c7c4b2eeb311e483f7aefacba069b4326f
신정희 <junghee6859@gmail.com>
2024-11-18 19:24:37 +0900

    feat : Remove test 관련 파일 제거
    test로 만들었던 파일 삭제

--- lib/provider/provider_set_up.dart
+++ lib/provider/provider_set_up.dart
@@ -1,10 +1,7 @@
 import 'package:app_front_talearnt/provider/auth/login_provider.dart';
-import 'package:app_front_talearnt/provider/test_provider.dart';
-import 'package:app_front_talearnt/view_model/test_view_model.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-import '../data/repositories/test_repository.dart';
 import 'common/common_provider.dart';
 
 class ProviderSetup extends StatelessWidget {
@@ -16,18 +13,6 @@ class ProviderSetup extends StatelessWidget {
   Widget build(BuildContext context) {
     return MultiProvider(
       providers: [
-        Provider<TestRepository>(
-          create: (_) => TestRepository(),
-        ),
-        ChangeNotifierProvider<TestProvider>(
-          create: (_) => TestProvider(),
-        ),
-        ChangeNotifierProvider<TestViewModel>(
-          create: (context) => TestViewModel(
-            Provider.of<TestProvider>(context, listen: false),
-            Provider.of<TestRepository>(context, listen: false),
-          ),
-        ),
         ChangeNotifierProvider<LoginProvider>(create: (_) => LoginProvider()),
         ChangeNotifierProvider<CommonProvider>(create: (_) => CommonProvider()),
       ],
