c7fb24afd7ad7234122574e564cec5d7a90d197e
신정희 <junghee6859@gmail.com>
2024-11-27 15:46:51 +0900

    feat : Update api URL 변경 및 추가
    url 변경 및 추가
    
    Ref : #172

--- lib/utils/api_constants.dart
+++ lib/utils/api_constants.dart
@@ -1,32 +1,28 @@
 abstract class ApiConstants {
   static const String baseUrl = "http://3.35.198.221";
-  static const String joinUrl = "$baseUrl/v1/api/join";
-  static const String joinVerifyCodeUrl = "$baseUrl/v1/api/join/verifyCode";
-  static const String joinSmsUrl = "$baseUrl/v1/api/join/sms";
-  static const String joinKakaoUrl = "$baseUrl/v1/api/join/kakao";
-  static const String refreshTokenUrl = "$baseUrl/v1/api/auth/refresh";
-  static const String loginUrl = "$baseUrl/v1/api/auth/login";
+  static const String joinUrl = "$baseUrl/v1/auth/join";
+  static const String joinKakaoUrl = "$baseUrl/v1/auth/join/kakao";
+  static const String refreshTokenUrl = "$baseUrl/v1/auth/login/refresh";
+  static const String loginUrl = "$baseUrl/v1/auth/login";
   static const String loginKakaoUrl = "$baseUrl/v1/api/auth/login/kakao";
-  static const String findUserIdCheckValidUrl =
-      "$baseUrl/v1/api/users/verification-code/validation";
-  static const String fineUserIdUrl = "$baseUrl/v1/api/users/user-id";
-  static const String activeAgreeCodeUrl =
-      "$baseUrl/v1/api/admin/agree-codes/active";
-  static const String setAgreeCodeUrl = "$baseUrl/v1/api/admin/agree-codes";
+  static const String smsVerifyCodeUrl =
+      "$baseUrl/v1/auth/sms/verification-codes"; // 인증번호 문자 전송 통합 url (회원가입, 아이디 찾기)
+  static const String smsValidUrl =
+      "$baseUrl/v1/auth/sms/validation"; //인증 번호 문자 검증 통합 url
 
-  static String getJoinDuplicationIdUrl(String userId) {
-    return "$baseUrl/v1/api/join/users/$userId";
-  }
+  static const String activeAgreeCodeUrl =
+      "$baseUrl/v1/admin/agree-codes/active";
+  static const String setAgreeCodeUrl = "$baseUrl/v1/admin/agree-codes";
+
+  static const String addTalentCategories = "$baseUrl/v1/admin/keywords/talent-categories";
+  static const String addBigCategories = "$baseUrl/v1/admin/keywords/big-categories";
+
 
   static String getFineUserPwUrl(String userId) {
-    return "$baseUrl/v1/api/users/$userId/email";
-  }
-
-  static String getFineUserIdSendValidUrl(String phone) {
-    return "$baseUrl/v1/api/users/$phone/verification-code";
+    return "$baseUrl/v1/auth/users/$userId/email";
   }
 
   static String getChangeUserPwUrl(String no, String uuid) {
-    return "$baseUrl/v1/api/users/$no/password/$uuid";
+    return "$baseUrl/v1/auth/users/$no/password/$uuid";
   }
 }
