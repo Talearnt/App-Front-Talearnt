d2b3c57b50f571eb1115e1d0e18e5067c399fb79
now-0o <dlwpdn0704@gmail.com>
2024-11-28 20:39:22 +0900

    fix : Update 공통 모듈 수정
    타이머 timer, timerSeconds 파라미터 받도록 수정
    
    Ref : #173

--- lib/provider/common/common_provider.dart
+++ lib/provider/common/common_provider.dart
@@ -8,11 +8,14 @@ class CommonProvider with ChangeNotifier {
   Timer? _timer;
 
   // 타이머 시작 메서드
-  void startTimer() {
+  void startTimer({required Timer timer, int? initialSeconds}) {
+    _timerSeconds = initialSeconds ?? _initialTimerSeconds;
+    _timer = timer;
+
     _timer = Timer.periodic(const Duration(seconds: 1), (timer) {
       if (_timerSeconds > 0) {
         _timerSeconds--;
-        notifyListeners(); // 타이머 값이 바뀔 때마다 UI에 알림
+        notifyListeners();
       } else {
         timer.cancel();
       }
