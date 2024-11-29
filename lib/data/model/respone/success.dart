6db126203443912870d1cb22fd0cdbbdca672edb
now-0o <dlwpdn0704@gmail.com>
2024-11-29 21:53:25 +0900

    feat : Add respone 파일 추가
    기본 요청 성공 시 사용할 success 파일 추가

---
+++ lib/data/model/respone/success.dart
@@ -0,0 +1,22 @@
+class Success {
+  final String? data;
+  final String? errorCode;
+  final String? errorMessage;
+  final bool success;
+
+  Success({
+    this.data,
+    required this.errorCode,
+    required this.errorMessage,
+    required this.success,
+  });
+
+  factory Success.fromJson(Map<String, dynamic> json) {
+    return Success(
+      data: json['data'],
+      errorCode: json['errorCode'],
+      errorMessage: json['errorMessage'],
+      success: json['success'],
+    );
+  }
+}
