c55b807da62aeb45beeadf947537bf13294336fd
신정희 <junghee6859@gmail.com>
2024-12-04 23:19:46 +0900

    feat : Add 자사 회원가입
    서버 체크하는 부분 base_text_field에 추가
    닉네임 가져오는 기능 추가
    닉네임 text_field에 정보성 문구 추가
    
    Fixes : #143

--- lib/view_model/auth_view_model.dart
+++ lib/view_model/auth_view_model.dart
@@ -1,5 +1,3 @@
-import 'package:app_front_talearnt/common/widget/button.dart';
-import 'package:app_front_talearnt/common/widget/dialog.dart';
 import 'package:app_front_talearnt/data/model/param/login_param.dart';
 import 'package:app_front_talearnt/data/model/param/send_cert_number_param.dart';
 import 'package:app_front_talearnt/data/model/param/send_reset_password_mail_param.dart';
@@ -10,6 +8,8 @@ import 'package:app_front_talearnt/provider/auth/sign_up_provider.dart';
 import 'package:app_front_talearnt/utils/token_manager.dart';
 import 'package:flutter/material.dart';
 
+import '../common/widget/button.dart';
+import '../common/widget/dialog.dart';
 import '../data/model/param/agree_req_dto.dart';
 import '../data/model/param/sign_up_param.dart';
 import '../data/model/param/sms_validation_param.dart';
@@ -32,6 +32,7 @@ class AuthViewModel extends ChangeNotifier {
     this.findPasswordProvider,
   );
 
+  // API 데이터 요청을 시뮬레이션하는 메서드
   Future<void> login(String email, String pw) async {
     LoginParam param = LoginParam(userId: email, pw: pw);
     final result = await authRepository.login(param);
