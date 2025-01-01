a1eca4ce3654931d88946a9cb019b2eda7956aa5
now-0o <dlwpdn0704@gmail.com>
2025-01-01 17:11:23 +0900

    feat : Add  게시글 에디터 기능추가
    비순서형 리스트 기능 추가
    순서형 리스트 기능 추가
    
    리스트 아이콘 추가
    
    Ref : #253

--- lib/view/talearnt_board/match_write2_page.dart
+++ lib/view/talearnt_board/match_write2_page.dart
@@ -2,7 +2,7 @@ import 'package:app_front_talearnt/common/theme.dart';
 import 'package:app_front_talearnt/common/widget/button.dart';
 import 'package:app_front_talearnt/common/widget/toast_message.dart';
 import 'package:app_front_talearnt/common/widget/top_app_bar.dart';
-import 'package:app_front_talearnt/provider/auth/match_write_provider.dart';
+import 'package:app_front_talearnt/provider/talearnt_board/match_write_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:go_router/go_router.dart';
@@ -133,6 +133,48 @@ class MatchWrite2Page extends StatelessWidget {
                           },
                         ),
                         IconButton(
+                          icon: matchWriteProvider.isUl
+                              ? SvgPicture.asset(
+                                  'assets/icons/list_after.svg',
+                                )
+                              : SvgPicture.asset(
+                                  'assets/icons/list_before.svg',
+                                ),
+                          onPressed: () {
+                            matchWriteProvider.isUl
+                                ? matchWriteProvider.contentController
+                                    .formatSelection(
+                                    Attribute.clone(Attribute.ul, null),
+                                  )
+                                : matchWriteProvider.contentController
+                                    .formatSelection(
+                                    Attribute.ul,
+                                  );
+                            matchWriteProvider.toggleButton("ul");
+                          },
+                        ),
+                        IconButton(
+                          icon: matchWriteProvider.isOl
+                              ? SvgPicture.asset(
+                                  'assets/icons/listnumber_after.svg',
+                                )
+                              : SvgPicture.asset(
+                                  'assets/icons/listnumber_before.svg',
+                                ),
+                          onPressed: () {
+                            matchWriteProvider.isOl
+                                ? matchWriteProvider.contentController
+                                    .formatSelection(
+                                    Attribute.clone(Attribute.ol, null),
+                                  )
+                                : matchWriteProvider.contentController
+                                    .formatSelection(
+                                    Attribute.ol,
+                                  );
+                            matchWriteProvider.toggleButton("ol");
+                          },
+                        ),
+                        IconButton(
                           icon: matchWriteProvider.alignType == "left"
                               ? SvgPicture.asset(
                                   'assets/icons/left_before.svg',
@@ -208,11 +250,54 @@ class MatchWrite2Page extends StatelessWidget {
                             'assets/icons/typo.svg',
                           ),
                         ),
-                        const SizedBox(
-                          width: 10,
-                        ),
-                        SvgPicture.asset(
-                          'assets/icons/left_before.svg',
+                        IconButton(
+                          icon: matchWriteProvider.alignType == "left"
+                              ? SvgPicture.asset(
+                                  'assets/icons/left_before.svg',
+                                )
+                              : matchWriteProvider.alignType == "center"
+                                  ? SvgPicture.asset(
+                                      'assets/icons/center_before.svg',
+                                    )
+                                  : matchWriteProvider.alignType == "right"
+                                      ? SvgPicture.asset(
+                                          'assets/icons/right_before.svg',
+                                        )
+                                      : SvgPicture.asset(
+                                          'assets/icons/justify_before.svg',
+                                        ),
+                          onPressed: () {
+                            if (matchWriteProvider.alignType == "left") {
+                              matchWriteProvider.contentController
+                                  .formatSelection(
+                                Attribute.centerAlignment,
+                              );
+                              matchWriteProvider.toggleButton(
+                                  "align", "center");
+                            } else if (matchWriteProvider.alignType ==
+                                "center") {
+                              matchWriteProvider.contentController
+                                  .formatSelection(
+                                Attribute.rightAlignment,
+                              );
+                              matchWriteProvider.toggleButton("align", "right");
+                            } else if (matchWriteProvider.alignType ==
+                                "right") {
+                              matchWriteProvider.contentController
+                                  .formatSelection(
+                                Attribute.justifyAlignment,
+                              );
+                              matchWriteProvider.toggleButton(
+                                  "align", "justify");
+                            } else if (matchWriteProvider.alignType ==
+                                "justify") {
+                              matchWriteProvider.contentController
+                                  .formatSelection(
+                                Attribute.leftAlignment,
+                              );
+                              matchWriteProvider.toggleButton("align", "left");
+                            }
+                          },
                         ),
                         Expanded(
                           child: Align(
