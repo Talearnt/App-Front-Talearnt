264992723daacd8459a96817f30cadaca510d7ea
now-0o <dlwpdn0704@gmail.com>
2025-03-13 21:57:45 +0900

    feat : Add 메인 페이지 UI
    게시글 CARD 매칭/커뮤니티 구분
    메인 페이지 UI 완료
    
    Ref : #406

--- lib/view/home_page.dart
+++ lib/view/home_page.dart
@@ -22,6 +22,9 @@ class HomePage extends StatelessWidget {
     final profileProvider = Provider.of<ProfileProvider>(context);
     final viewModel = Provider.of<BoardViewModel>(context);
 
+    final List<String> matchBoardList =
+        List.generate(10, (index) => 'Match $index');
+
     return Scaffold(
       bottomNavigationBar: Container(
         decoration: const BoxDecoration(
@@ -116,176 +119,318 @@ class HomePage extends StatelessWidget {
           ),
         ),
       ),
-      body: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.symmetric(
-              horizontal: 20,
-              vertical: 16,
-            ),
-            child: Row(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.only(
-                    top: 1,
-                    bottom: 1,
-                    left: 8,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 20,
+                vertical: 16,
+              ),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(
+                      top: 1,
+                      bottom: 1,
+                      left: 8,
+                    ),
+                    child: SvgPicture.asset(
+                      'assets/icons/default_logo.svg',
+                      width: 112,
+                      height: 22,
+                    ),
                   ),
+                  SvgPicture.asset(
+                    'assets/icons/bell_off.svg',
+                    width: 18,
+                    height: 20,
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              width: double.infinity,
+              child: AspectRatio(
+                aspectRatio: 375 / 188,
+                child: FittedBox(
+                  fit: BoxFit.cover,
                   child: SvgPicture.asset(
-                    'assets/icons/default_logo.svg',
-                    width: 112,
-                    height: 22,
+                    'assets/img/main_banner1.svg',
+                    width: MediaQuery.of(context).size.width,
                   ),
-                ),
-                SvgPicture.asset(
-                  'assets/icons/bell_off.svg',
-                  width: 18,
-                  height: 20,
-                ),
-              ],
-            ),
-          ),
-          SizedBox(
-            width: double.infinity,
-            child: AspectRatio(
-              aspectRatio: 375 / 188,
-              child: FittedBox(
-                fit: BoxFit.cover,
-                child: SvgPicture.asset(
-                  'assets/img/main_banner1.svg',
-                  width: MediaQuery.of(context).size.width,
                 ),
               ),
             ),
-          ),
-          const SizedBox(
-            height: 36,
-          ),
-          loginProvider.isLoggedIn
-              ? Column(
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.symmetric(
-                        horizontal: 24,
-                      ),
-                      child: Align(
-                        alignment: Alignment.centerLeft,
-                        child: Text(
-                          "관심있는 키워드를 반영했어요",
-                          style: TextTypes.captionMedium02(
-                            color: Palette.text02,
+            const SizedBox(
+              height: 36,
+            ),
+            loginProvider.isLoggedIn
+                ? Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.symmetric(
+                          horizontal: 24,
+                        ),
+                        child: Align(
+                          alignment: Alignment.centerLeft,
+                          child: Text(
+                            "관심있는 키워드를 반영했어요",
+                            style: TextTypes.captionMedium02(
+                              color: Palette.text02,
+                            ),
                           ),
                         ),
                       ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(
-                        horizontal: 24,
-                      ),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text.rich(
-                            TextSpan(
-                              text: profileProvider.userProfile.nickname,
-                              style: TextTypes.bodySemi01(
-                                color: Palette.primary01,
+                      Padding(
+                        padding: const EdgeInsets.symmetric(
+                          horizontal: 24,
+                        ),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text.rich(
+                              TextSpan(
+                                text: profileProvider.userProfile.nickname,
+                                style: TextTypes.bodySemi01(
+                                  color: Palette.primary01,
+                                ),
+                                children: <TextSpan>[
+                                  TextSpan(
+                                    text: '님을 위한 맞춤 매칭',
+                                    style: TextTypes.bodySemi01(
+                                      color: Palette.text01,
+                                    ),
+                                  ),
+                                ],
                               ),
-                              children: <TextSpan>[
-                                TextSpan(
-                                  text: '님을 위한 맞춤 매칭',
-                                  style: TextTypes.bodySemi01(
-                                    color: Palette.text01,
-                                  ),
-                                ),
-                              ],
                             ),
-                          ),
-                          GestureDetector(
-                            onTap: () {},
-                            child: Row(
-                              mainAxisSize: MainAxisSize.min,
-                              children: [
-                                Text(
-                                  '더보기',
-                                  style: TextTypes.caption01(
-                                    color: Palette.text03,
+                            GestureDetector(
+                              onTap: () {},
+                              child: Row(
+                                mainAxisSize: MainAxisSize.min,
+                                children: [
+                                  Text(
+                                    '더보기',
+                                    style: TextTypes.caption01(
+                                      color: Palette.text03,
+                                    ),
                                   ),
-                                ),
-                                SvgPicture.asset(
-                                  'assets/icons/add_more_arrow.svg',
-                                  width: 24,
-                                  height: 24,
-                                ),
-                              ],
+                                  SvgPicture.asset(
+                                    'assets/icons/add_more_arrow.svg',
+                                    width: 24,
+                                    height: 24,
+                                  ),
+                                ],
+                              ),
                             ),
-                          ),
-                        ],
+                          ],
+                        ),
                       ),
-                    ),
-                    const SizedBox(
-                      height: 24,
-                    ),
-                    const HomeBoardCard(),
-                  ],
-                )
-              : Column(
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.symmetric(
-                        horizontal: 24,
+                      const SizedBox(
+                        height: 12,
                       ),
-                      child: Align(
-                        alignment: Alignment.centerLeft,
-                        child: Text(
-                          "단 3초! 로그인하고 확인해보세요",
-                          style: TextTypes.captionMedium02(
-                            color: Palette.text02,
+                      SingleChildScrollView(
+                        scrollDirection: Axis.horizontal,
+                        padding: const EdgeInsets.symmetric(vertical: 12),
+                        child: Row(
+                          children: [
+                            const SizedBox(width: 24),
+                            ...matchBoardList.map((match) {
+                              return const Padding(
+                                padding: EdgeInsets.only(right: 14),
+                                child: HomeMatchBoardCard(),
+                              );
+                            }).toList(),
+                          ],
+                        ),
+                      )
+                    ],
+                  )
+                : Column(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.symmetric(
+                          horizontal: 24,
+                        ),
+                        child: Align(
+                          alignment: Alignment.centerLeft,
+                          child: Text(
+                            "단 3초! 로그인하고 확인해보세요",
+                            style: TextTypes.captionMedium02(
+                              color: Palette.text02,
+                            ),
                           ),
                         ),
                       ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(
-                        horizontal: 24,
-                      ),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text.rich(
-                            TextSpan(
-                              text: '회원',
-                              style: TextTypes.bodySemi01(
-                                color: Palette.primary01,
-                              ),
-                              children: <TextSpan>[
-                                TextSpan(
-                                  text: '님을 위한 맞춤 매칭',
-                                  style: TextTypes.bodySemi01(
-                                    color: Palette.text01,
-                                  ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(
+                          horizontal: 24,
+                        ),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text.rich(
+                              TextSpan(
+                                text: '회원',
+                                style: TextTypes.bodySemi01(
+                                  color: Palette.primary01,
                                 ),
-                              ],
+                                children: <TextSpan>[
+                                  TextSpan(
+                                    text: '님을 위한 맞춤 매칭',
+                                    style: TextTypes.bodySemi01(
+                                      color: Palette.text01,
+                                    ),
+                                  ),
+                                ],
+                              ),
                             ),
-                          ),
-                          const PrimaryS(
-                            content: "로그인",
-                          ),
-                        ],
+                            const PrimaryS(
+                              content: "로그인",
+                            ),
+                          ],
+                        ),
                       ),
-                    ),
-                    const SizedBox(
-                      height: 24,
-                    ),
-                    const Divider(
-                      thickness: 8,
-                      color: Palette.line02,
-                    )
-                  ],
+                      const SizedBox(
+                        height: 24,
+                      ),
+                      const Divider(
+                        thickness: 8,
+                        color: Palette.line02,
+                      )
+                    ],
+                  ),
+            const SizedBox(
+              height: 44,
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 24,
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        '신규 매칭 게시물이 올라왔어요!',
+                        style: TextTypes.bodySemi01(
+                          color: Palette.text01,
+                        ),
+                      ),
+                      GestureDetector(
+                        onTap: () {},
+                        child: Row(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            Text(
+                              '더보기',
+                              style: TextTypes.caption01(
+                                color: Palette.text03,
+                              ),
+                            ),
+                            SvgPicture.asset(
+                              'assets/icons/add_more_arrow.svg',
+                              width: 24,
+                              height: 24,
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
-        ],
+                const SizedBox(
+                  height: 12,
+                ),
+                SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  padding: const EdgeInsets.symmetric(vertical: 12),
+                  child: Row(
+                    children: [
+                      const SizedBox(width: 24),
+                      ...matchBoardList.map((match) {
+                        return const Padding(
+                          padding: EdgeInsets.only(right: 14),
+                          child: HomeMatchBoardCard(),
+                        );
+                      }).toList(),
+                    ],
+                  ),
+                )
+              ],
+            ),
+            const SizedBox(
+              height: 44,
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 24,
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        'BEST 커뮤니티 글만 모아봤어요!',
+                        style: TextTypes.bodySemi01(
+                          color: Palette.text01,
+                        ),
+                      ),
+                      GestureDetector(
+                        onTap: () {},
+                        child: Row(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            Text(
+                              '더보기',
+                              style: TextTypes.caption01(
+                                color: Palette.text03,
+                              ),
+                            ),
+                            SvgPicture.asset(
+                              'assets/icons/add_more_arrow.svg',
+                              width: 24,
+                              height: 24,
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(
+                  height: 12,
+                ),
+                SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  padding: const EdgeInsets.symmetric(vertical: 12),
+                  child: Row(
+                    children: [
+                      const SizedBox(width: 24),
+                      ...matchBoardList.map((match) {
+                        return const Padding(
+                          padding: EdgeInsets.only(right: 14),
+                          child: HomeCommunityCard(),
+                        );
+                      }).toList(),
+                    ],
+                  ),
+                )
+              ],
+            ),
+            const SizedBox(
+              height: 16,
+            ),
+          ],
+        ),
       ),
     );
   }
