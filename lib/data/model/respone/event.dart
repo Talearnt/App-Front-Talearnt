5b31fc5b596d7b54a48a3ddd304f7bf770fa3e64
now-0o <dlwpdn0704@gmail.com>
2025-09-23 21:39:12 +0900

    fix : Update 이벤트 에러 처리
    이벤트 endDate null 값으로 나던 에러 수정
    
    Ref : #502

--- lib/data/model/respone/event.dart
+++ lib/data/model/respone/event.dart
@@ -18,8 +18,14 @@ class Event {
   factory Event.fromJson(Map<String, dynamic> json) => Event(
         eventNo: json['eventNo'] as int,
         bannerUrl: json['bannerUrl'] as String,
-        startDate: DateTime.parse(json['startDate'] as String),
-        endDate: DateTime.parse(json['endDate'] as String),
+        startDate: json['startDate'] != null &&
+                (json['startDate'] as String).isNotEmpty
+            ? DateTime.parse(json['startDate'] as String)
+            : DateTime.now(),
+        endDate:
+            json['endDate'] != null && (json['endDate'] as String).isNotEmpty
+                ? DateTime.parse(json['endDate'] as String)
+                : DateTime.now(),
         isActive: json['isActive'] as bool,
       );
 
